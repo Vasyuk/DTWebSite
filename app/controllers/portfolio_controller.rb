b1f98ae53c64b72e4d1a5e6fe0cd4de15ed9c5fd
Vasyuk <misha2018@gmail.com>
2017-09-15 12:49:44 +0200

    Porfolio

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,21 +1,27 @@
 class PortfolioController < ApplicationController
 
   def background
+    @potfolio = Portfolio.all
   end
 
   def illustration
+    @potfolio = Portfolio.all
   end
 
   def modeles3D
+    @potfolio = Portfolio.all
   end
 
   def personnages
+    @potfolio = Portfolio.all
   end
 
   def photographie
+    @potfolio = Portfolio.all
   end
 
   def video
+    @potfolio = Portfolio.all  
   end
 
 end
