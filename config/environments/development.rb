3d6427837404867b8019d07f0131e4d07baffdab
Vasyuk <misha2018@gmail.com>
2017-10-03 17:18:27 +0200

    try1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,23 +33,16 @@ Rails.application.configure do
 
   config.action_dispatch.best_standards_support = :builtin
 
-
-
-  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => "localhost:3000" }
-
-
-ActionMailer::Base.smtp_settings = {
-                    :address        => "smtp.gmail.com",
-                    :port           => 587,
-                    :authentication => :plain,
-                    :user_name      => "misha2018@gmail.com",
-                    :password       => "Paris,51"
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => "misha2018@gmail.com",
+ :password             => "Paris,51",
+ :authentication       => "plain",
+:enable_starttls_auto => true
 }
-
-
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "DTWebSite_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'misha2018@gmail.com',
-    :password       => 'Paris,51',
-    :domain         => 'heroku.com'
-  }
 
-  ActionMailer::Base.delivery_method = :smtp
-
-  config.action_mailer.default_url_options = { :host => 'calm-oasis-72009.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+# SMTP settings for gmail
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => "misha2018@gmail.com",
+ :password             => "Paris,51",
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
